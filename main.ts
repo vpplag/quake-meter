4eb614dfe911d5301834c66a2b84fe438e59680e
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-11 20:03:21 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+function SetColor (red: number, green: number, blue: number) {
+    red = Math.map(red, 0, 255, 1023, 0)
+    green = Math.map(green, 0, 255, 1023, 0)
+    blue = Math.map(blue, 0, 255, 1023, 0)
+    pins.analogWritePin(AnalogPin.P0, red)
+    pins.analogWritePin(AnalogPin.P1, green)
+    pins.analogWritePin(AnalogPin.P2, blue)
+    basic.pause(200)
+}
+let blue = 0
+let green = 0
+let red = 0
+input.setAccelerometerRange(AcceleratorRange.OneG)
+basic.forever(function () {
+    if (Math.abs(input.acceleration(Dimension.Strength) - 1000) > 100) {
+        red = 255
+        green = 0
+        blue = 0
+        SetColor(red, green, blue)
+    } else {
+        red = 0
+        green = 255
+        blue = 0
+        SetColor(red, green, blue)
+    }
+})
